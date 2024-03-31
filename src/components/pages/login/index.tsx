5fe817f089697ee2d3ad3386d549cc9350954a5d
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-03-31 19:32:21 +0900

    react-hook-form + zod

---
+++ src/components/pages/login/index.tsx
@@ -0,0 +1,113 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signUpSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(10, "비밀번호는 최소 10자리 이상이어야 합니다."),
+    chkPassword: z.string(),
+  })
+  .refine((data) => data.password === data.chkPassword, {
+    message: "패스워드는 성욀해야 합니다.",
+    path: ["chkPassword"],
+  });
+
+// type FormValue = {
+//   email: string;
+//   password: string;
+//   chkPassword: string;
+// };
+type FormValue = z.infer<typeof signUpSchema>;
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    getValues,
+    reset,
+  } = useForm<FormValue>({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  const onSubmit = async (data: FormValue) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                {...register("email")}
+                type="email"
+                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors.email && <p className="text-red-500">{errors?.email?.message}</p>}
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                {...register("password")}
+                type="password"
+                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                {...register("chkPassword")}
+                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors.chkPassword && <p className="text-red-500">{errors.chkPassword.message}</p>}
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, createRoutesFromElements, createBrowserRouter } from "react-router-dom";
 import Home from "../components/pages/home";
 import Quiz from "../components/pages/quiz";
+import Login from "../components/pages/login";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Home />} />
       <Route path="/quiz" element={<Quiz />} />
+      <Route path="/login" element={<Login />} />
     </Route>
   )
 );
